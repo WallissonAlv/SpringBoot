559d60a787b67afdf432e1b8f7375475896ad463
Wallisson Alves <118410826+WallissonAlv@users.noreply.github.com>
2024-01-22 11:57:49 -0300

    Delete Remedios directory

--- Remedios/Curso/src/main/java/com/remedios/wallisson/curso/remedio/Laboratorio.java
+++
@@ -1,7 +0,0 @@
-package com.remedios.wallisson.curso.remedio;
-
-public enum Laboratorio {
-
-	MEDLEY,
-	ACHE
-}
--- Remedios/Curso/src/main/java/com/remedios/wallisson/curso/remedio/Via.java
+++
@@ -1,9 +0,0 @@
-package com.remedios.wallisson.curso.remedio;
-
-public enum Via {
-	ORAL,
-	NASAL,
-	VENOSO,
-	INTRAMUSCULAR,
-	RETAL
-}
