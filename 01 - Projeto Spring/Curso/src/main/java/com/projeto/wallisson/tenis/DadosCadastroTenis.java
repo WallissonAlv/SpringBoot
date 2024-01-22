ebb1b4e48d3f8c7f213be84f63998777f91b39b4
Wallisson Alves <118410826+WallissonAlv@users.noreply.github.com>
2024-01-22 11:58:18 -0300

    Delete 01 - Projeto Spring directory

--- 01 - Projeto Spring/Curso/src/main/java/com/projeto/wallisson/tenis/DadosCadastroTenis.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.wallisson.tenis;
-
-public record DadosCadastroTenis(String nome,
-		String tamanhous, String tamanhobr,
-		String cabedal, tipo tipo,
-		transportadora transportadora) {
-
-}
--- 01 - Projeto Spring/Curso/src/main/java/com/projeto/wallisson/tenis/tipo.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.wallisson.tenis;
-
-public enum tipo {
-	WOMEN,
-	MEN,
-	KIDS,
-	UNISSEX
-}
