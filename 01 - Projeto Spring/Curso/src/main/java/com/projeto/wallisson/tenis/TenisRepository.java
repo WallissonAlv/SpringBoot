d1e8df559bfe426cb18c37eeea5fc227d4419edf
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-22 16:46:28 -0300

    .

--- 01 - Projeto Spring/Curso/src/main/java/com/projeto/wallisson/tenis/TenisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.wallisson.tenis;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TenisRepository extends JpaRepository <Tenis,Long>{ 
-
-}
--- Remedios/Curso/src/main/java/com/remedios/wallisson/curso/remedio/RemedioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.remedios.wallisson.curso.remedio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RemedioRepository extends JpaRepository<Remedio, Long>{
-	
-}
